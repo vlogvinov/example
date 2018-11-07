455d43a3f7c9a75eba392f44a9a2f17cf4a6d2c9
v.logvinov <volodymyr.logvinov@gmail.com>
2018-11-07 16:34:24 +0200

    init commit

---
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.demoqa.com";
+            driver.Close();
+        }
+    }
+}
